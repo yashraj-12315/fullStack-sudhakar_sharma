2868d0a86398e69d6b4f4ceb490976a5c2fdbe3e
Yash Raj <yashraj4586@gmail.com>
2025-06-30 19:21:31 +0530

    added more functionalities

--- src/scripts/toDo.js
+++ src/scripts/toDo.js
@@ -39,6 +39,26 @@ function sortDesc() {
 function deleteClick() {
     var selectedAppointment = document.getElementById("lstAppointments").value;
     var selectedIndex = appointments.indexOf(selectedAppointment);
-    appointments.splice(selectedIndex, 1);
+    var choice = confirm(`Are you sure?\nWant to Delete?\n${selectedAppointment}`)
+    if (choice === true) {
+        appointments.splice(selectedIndex, 1);
+        loadAppointments();
+    }
+}
+
+function clearAll() {
+    appointments.length = 0;
+    loadAppointments();
+}
+
+function editClick() {
+    document.getElementById("txtEdit").value = document.getElementById("lstAppointments").value;
+}
+
+function saveClick() {
+    var selectedAppointment = document.getElementById("lstAppointments").value;
+    var selectedIndex = appointments.indexOf(selectedAppointment);
+    var newAppointment = document.getElementById("txtEdit").value;
+    appointments[selectedIndex] = newAppointment
     loadAppointments();
 }
